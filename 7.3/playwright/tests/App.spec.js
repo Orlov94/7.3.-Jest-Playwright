336bea07d3020ef080ec278a9cf30e08b5151e5e
Vladislav <vladislav_orlov_94@mail.ru>
2022-11-12 17:35:53 +0300

    update playwright

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -9,13 +9,18 @@ test("Successful authorization", async () => {
   });
   const page = await browser.newPage("https://netology.ru/");
   await page.goto("https://netology.ru/");
+
   await page.getByRole("link", { name: "Войти" }).click();
   await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+
   await page.getByPlaceholder("Email").click();
   await page.getByPlaceholder("Email").fill(email);
   await page.getByPlaceholder("Пароль").click();
   await page.getByPlaceholder("Пароль").fill(password);
   await page.getByTestId("login-submit-btn").click();
+
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  await expect(page.locator("h2")).toHaveText("Мои курсы и профессии");
   browser.close();
 }, 60000);
 
@@ -26,14 +31,18 @@ test("Failed authorization", async () => {
   });
   const page = await browser.newPage("https://netology.ru/");
   await page.goto("https://netology.ru/");
+
   await page.getByRole("link", { name: "Войти" }).click();
   await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+
   await page.getByPlaceholder("Email").click();
   await page.getByPlaceholder("Email").fill(email);
   await page.getByPlaceholder("Пароль").click();
   await page.getByPlaceholder("Пароль").fill(negativPassword);
   await page.getByTestId("login-submit-btn").click();
+
   const error = await page.locator('[data-testid="login-error-hint"]');
   await expect(error).toHaveText("Вы ввели неправильно логин или пароль");
+
   browser.close();
 }, 60000);
