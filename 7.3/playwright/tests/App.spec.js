44082696f2d6b5c75aa2fa844acd4a2e65ae8ef9
Vladislav <vladislav_orlov_94@mail.ru>
2022-11-12 16:37:14 +0300

    add playwright tests

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -1,16 +1,39 @@
 const { test, expect } = require("@playwright/test");
 const { chromium } = require("playwright");
-const { email, password } = require("../user");
+const { email, password, negativPassword } = require("../user");
 
 test("Successful authorization", async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 500,
   });
-  const page = await browser.newPage("https://netology.ru/?modal=sign_in");
-  await page.goto("https://netology.ru/?modal=sign_in");
-  await page.fill('[placeholder="Email"]', email);
-  await page.fill('[placeholder="Пароль"]', password);
-  await page.click('[data-testid="login-submit-btn"]');
+  const page = await browser.newPage("https://netology.ru/");
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+  browser.close();
+}, 60000);
+
+test("Failed authorization", async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 500,
+  });
+  const page = await browser.newPage("https://netology.ru/");
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(negativPassword);
+  await page.getByTestId("login-submit-btn").click();
+  const error = await page.locator('[data-testid="login-error-hint"]');
+  await expect(error).toHaveText("Вы ввели неправильно логин или пароль");
   browser.close();
 }, 60000);
